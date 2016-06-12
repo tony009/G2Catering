65805386b7ef04addba5531d10e99d10a38742d4
Damon Wu <qjwdcool@qq.com>
2016-06-12 19:16:16 +0800

    1.增加pch文件，增加网络库

---
+++ G2Catering/G2Catering/Expand(拓展)/Network(网络)/HttpTool.h
@@ -0,0 +1,48 @@
+//
+//  HttpTool.h
+//  RuiMu
+//
+//  Created by wd on 16/4/25.
+//  Copyright © 2016年 NDL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface HttpTool : NSObject
+
+typedef void (^success)(id response);
+typedef void (^failure)(NSError *error);
+
+
++(void)startNetworkMonitoring;
+
++(BOOL)isConnectionAvailable;
+
++ (void)POST:(NSString *)URLString
+  parameters:(id)parameters
+     success:(success)success
+     failure:(failure)failure;
+
++(void)GET:(NSString *)URLString
+parameters:(id)parameters
+   success:(success)success
+   failure:(failure)failure;
+
+
++(void)getWithForm:(NSString *)restPath
+       parameters:(id)parameters
+        modelClass:(Class)modelClass
+           keyPath:(NSString *)keyPath
+           success:(success)success
+           failure:(failure)failure;
+
+
++(void)postWithForm:(NSString *)restPath
+         parameters:(id)parameters
+         modelClass:(Class)modelClass
+            keyPath:(NSString *)keyPath
+            success:(success)success
+            failure:(failure)failure;
+
+@end
