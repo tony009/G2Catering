ff167586b8c73ec7f3246e02723f37efb6ba8a57
Damon Wu <qjwdcool@qq.com>
2016-06-12 10:16:40 +0800

    init

---
+++ G2Catering/G2Catering/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  G2Catering
+//
+//  Created by wd on 16/6/12.
+//  Copyright © 2016年 NDL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ G2Catering/G2CateringTests/G2CateringTests.m
@@ -0,0 +1,39 @@
+//
+//  G2CateringTests.m
+//  G2CateringTests
+//
+//  Created by wd on 16/6/12.
+//  Copyright © 2016年 NDL. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface G2CateringTests : XCTestCase
+
+@end
+
+@implementation G2CateringTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
