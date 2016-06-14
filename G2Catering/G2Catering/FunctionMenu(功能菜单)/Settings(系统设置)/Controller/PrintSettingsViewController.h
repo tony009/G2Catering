068f08996cec7828a70caa0f2133358a636b9fa5
Damon Wu <qjwdcool@qq.com>
2016-06-14 10:41:50 +0800

    1.更新目录

---
+++ G2Catering/G2Catering/FunctionMenu(功能菜单)/Settings(系统设置)/Controller/PrintSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  PrintSettingsViewController.h
+//  G2Catering
+//
+//  Created by wd on 16/6/14.
+//  Copyright © 2016年 NDL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PrintSettingsViewController : UIViewController
+
+@end
