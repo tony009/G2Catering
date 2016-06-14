aef0b1e810d0a5cc7a9b2ae88494c7b9beb30207
Damon Wu <qjwdcool@qq.com>
2016-06-14 10:29:09 +0800

    1.更新目录

---
+++ G2Catering/G2Catering/Main(主界面)/Ordering(点单)/View/OrderingCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  OrderingCollectionViewCell.h
+//  G2Catering
+//
+//  Created by wd on 16/6/12.
+//  Copyright © 2016年 NDL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrderingCollectionViewCell : UICollectionViewCell
+
+@end
