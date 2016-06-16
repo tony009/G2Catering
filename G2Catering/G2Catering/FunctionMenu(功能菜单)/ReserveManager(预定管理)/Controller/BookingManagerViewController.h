3b54981f400470a00c772af2eaeb6eeba3893581
刘明 <liuming@ndlan.com>
2016-06-16 12:36:08 +0800

    1.修改预定

--- G2Catering/G2Catering/FunctionMenu(功能菜单)/ReserveManager(预定管理)/Controller/BookingManagerViewController.h
+++ G2Catering/G2Catering/FunctionMenu(功能菜单)/ReserveManager(预定管理)/Controller/BookingManagerViewController.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface BookingManagerViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIView *searchBackView;
+@property (weak, nonatomic) IBOutlet UITextField *searchTextField;
+@property (weak, nonatomic) IBOutlet UIButton *addBtn;
+@property (weak, nonatomic) IBOutlet UITableView *bookTableView;
 
 @end
