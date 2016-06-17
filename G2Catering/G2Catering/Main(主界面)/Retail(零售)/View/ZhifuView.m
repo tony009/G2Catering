f58d9445bc88fd4f48bbfdbc2c5081aad697deea
baibianmoka <iOS@tengshimatoimac.local>
2016-06-17 15:47:01 +0800

    j交易记录界面的修改

---
+++ G2Catering/G2Catering/Main(主界面)/Retail(零售)/View/ZhifuView.m
@@ -0,0 +1,21 @@
+//
+//  ZhifuView.m
+//  G2Catering
+//
+//  Created by iOS  on 16/6/17.
+//  Copyright © 2016年 NDL. All rights reserved.
+//
+
+#import "ZhifuView.h"
+
+@implementation ZhifuView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
