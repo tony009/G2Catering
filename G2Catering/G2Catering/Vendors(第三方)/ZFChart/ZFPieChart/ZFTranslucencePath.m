764a1e711cd19c221a12dcf16ea77ab7d0e52c48
刘明 <liuming@ndlan.com>
2016-06-28 17:55:17 +0800

    加入第三方:图形

---
+++ G2Catering/G2Catering/Vendors(第三方)/ZFChart/ZFPieChart/ZFTranslucencePath.m
@@ -0,0 +1,34 @@
+//
+//  ZFTranslucencePath.m
+//  ZFChartView
+//
+//  Created by apple on 16/2/23.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import "ZFTranslucencePath.h"
+#import "ZFConst.h"
+
+@implementation ZFTranslucencePath
+
++ (instancetype)layerWithArcCenter:(CGPoint)center radius:(CGFloat)radius startAngle:(CGFloat)startAngle endAngle:(CGFloat)endAngle clockwise:(BOOL)clockwise{
+    return [[ZFTranslucencePath alloc] initWithArcCenter:center radius:radius startAngle:startAngle endAngle:endAngle clockwise:clockwise];
+}
+
+- (instancetype)initWithArcCenter:(CGPoint)center radius:(CGFloat)radius startAngle:(CGFloat)startAngle endAngle:(CGFloat)endAngle clockwise:(BOOL)clockwise{
+    self = [super init];
+    if (self) {
+        self.fillColor = nil;
+        //刘明修改透明度 原来为0.5
+        self.opacity = 1;
+        self.path = [self translucencePathWithArcCenter:center radius:radius startAngle:startAngle endAngle:endAngle clockwise:clockwise].CGPath;
+    }
+    return self;
+}
+
+- (UIBezierPath *)translucencePathWithArcCenter:(CGPoint)center radius:(CGFloat)radius startAngle:(CGFloat)startAngle endAngle:(CGFloat)endAngle clockwise:(BOOL)clockwise{
+    UIBezierPath * bezierPath = [UIBezierPath bezierPathWithArcCenter:center radius:radius startAngle:startAngle endAngle:endAngle clockwise:clockwise];
+    return bezierPath;
+}
+
+@end
