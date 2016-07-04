d3bb521fc555b2abc72c733f935d3a37ec662baa
manmanking <ws@idontknow.local>
2016-07-04 10:43:33 +0800

    添加支付界面

---
+++ G2Catering/G2Catering/payment(支付)/controller/PaymentViewController.h
@@ -0,0 +1,13 @@
+//
+//  PaymentViewController.h
+//  G2Catering
+//
+//  Created by ws on 16/7/4.
+//  Copyright © 2016年 NDL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaymentViewController : UIViewController
+
+@end
